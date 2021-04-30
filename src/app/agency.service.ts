0a91f3ed1728becbee86f702deb32cef8a33e78a
MiguelManrique <m.manriquealejos@gmail.com>
2021-04-30 00:54:01 -0500

    Small fixes and added local storage update
    BCP logo button returns to loading page, Added functionality in service for updating the agency data and added local storage updates too.

--- src/app/agency.service.ts
+++ src/app/agency.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import agencias from './_files/agencias.json'
-
+import {Agency} from './_models/Agency'
 @Injectable({
   providedIn: 'root'
 })
 export class AgencyService {
 
-  listaAgencias : {agencia: string, distrito: string, provincia: string, departamento: string, lat: number, lon: number}[]=agencias;
-  constructor() { }
+  listaAgencias! : Agency[];
+  constructor() {
+    this.listaAgencias = agencias;
+   }
 
   getAgencia(name: string) {
     for(let i=0; i<this.listaAgencias.length; i++){
@@ -20,6 +22,28 @@ export class AgencyService {
    }
 
   getAgencias() {
+    if(localStorage.getItem('data') === null){
+      return this.listaAgencias;
+    }else{
+      this.listaAgencias = JSON.parse(localStorage.getItem('data') || '[]')
+    }
     return this.listaAgencias;
+  }
+
+  updateAgencia(agencia: Agency,num: number){
+    for(let i=0; i<this.listaAgencias.length; i++){
+      if(i == num){
+
+        this.listaAgencias[i].agencia = agencia.agencia;
+        this.listaAgencias[i].distrito = agencia.distrito;
+        this.listaAgencias[i].provincia = agencia.provincia;
+        this.listaAgencias[i].departamento = agencia.departamento;
+        this.listaAgencias[i].direccion = agencia.direccion;
+        this.listaAgencias[i].lat= agencia.lat;
+        this.listaAgencias[i].lon= agencia.lon;
+      }
+    }
+    //Aqui se modifica el local storage
+    localStorage.setItem('data',JSON.stringify(this.listaAgencias));
   }
 }
--- src/app/agency/agency.component.ts
+++ src/app/agency/agency.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AgencyService } from '../agency.service';
+import { Agency } from '../_models/Agency'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-agency',
@@ -9,22 +11,40 @@ import { AgencyService } from '../agency.service';
 })
 export class AgencyComponent implements OnInit {
   agency: any;
+  agencies: any;
+  numAgency: any;
 
   constructor(private agencyService: AgencyService,
+      private router: Router,
       private route: ActivatedRoute
     ) { 
       this.route.params.subscribe(params =>{
         const agencianame = params['agencianame'];
         this.agency = this.agencyService.getAgencia(agencianame);
         console.log(this.agency);
-      }); 
+      });
+      this.agencies = this.agencyService.getAgencias();
+      for(let i=0; i<this.agencies.length; i++){
+        if(this.agencies[i].agencia == this.agency.agencia){
+          this.numAgency = i;
+        }
+      }
     }
 
   ngOnInit(): void { 
   }
 
-  submitForm(){
+  submitForm(agency: Agency){
     alert('Actualizado!');
-  }
+    agency.agencia = this.agency.agencia;
+    agency.distrito = this.agency.distrito;
+    agency.provincia = this.agency.provincia;
+    agency.departamento = this.agency.departamento;
+    agency.direccion = this.agency.direccion;
+    agency.lat = this.agency.lat;
+    agency.lon = this.agency.lon;
 
+    this.agencyService.updateAgencia(agency,this.numAgency);
+    this.router.navigate([`/agencias`]);
+  }
 }
